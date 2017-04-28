298b7b1e6d634f8db27e64b4b3ae2bf94717e551
Rayzr522 <rayzr522@gmail.com>
2017-04-28 11:58:31 -0400

    Removed CNL and set up config

--- src/main/java/com/rayzr522/commandtimer/CommandTimer.java
+++ src/main/java/com/rayzr522/commandtimer/CommandTimer.java
@@ -1,13 +1,6 @@
 package com.rayzr522.commandtimer;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.rayzr522.creativelynamedlib.config.Messages;
 
 /**
  * @author Rayzr
@@ -15,7 +8,9 @@ import com.rayzr522.creativelynamedlib.config.Messages;
 public class CommandTimer extends JavaPlugin {
     private static CommandTimer instance;
 
-    private Messages lang = new Messages();
+    public static CommandTimer getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
@@ -23,88 +18,18 @@ public class CommandTimer extends JavaPlugin {
 
         reload();
     }
-    
+
     @Override
     public void onDisable() {
         instance = null;
     }
-    
+
     /**
      * (Re)loads all configs from the disk
      */
     public void reload() {
         saveDefaultConfig();
         reloadConfig();
-        
-        lang.load(getConfig("messages.yml"));
-    }
-
-    /**
-     * If the file is not found and there is a default file in the JAR, it saves the default file to the plugin data folder first
-     * 
-     * @param path The path to the config file (relative to the plugin data folder)
-     * @return The {@link YamlConfiguration}
-     */
-    public YamlConfiguration getConfig(String path) {
-        if (!getFile(path).exists() && getResource(path) != null) {
-            saveResource(path, true);
-        }
-        return YamlConfiguration.loadConfiguration(getFile(path));
-    }
-    
-    /**
-     * Attempts to save a {@link YamlConfiguration} to the disk, and any {@link IOException}s are printed to the console
-     * 
-     * @param config The config to save
-     * @param path The path to save the config file to (relative to the plugin data folder)
-     */
-    public void saveConfig(YamlConfiguration config, String path) {
-        try {
-            config.save(getFile(path));
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Failed to save config", e);
-        }
-    }
-
-    /**
-     * @param path The path of the file (relative to the plugin data folder)
-     * @return The {@link File}
-     */
-    public File getFile(String path) {
-        return new File(getDataFolder(), path.replace('/', File.pathSeparatorChar));
-    }
-    
-    /**
-     * Returns a message from the language file
-     * 
-     * @param key The key of the message to translate
-     * @param objects The formatting objects to use
-     * @return The formatted message
-     */
-    public String tr(String key, Object... objects) {
-        return lang.tr(key, objects);
-    }
-
-    /**
-     * Returns a message from the language file without adding the prefix
-     * 
-     * @param key The key of the message to translate
-     * @param objects The formatting objects to use
-     * @return The formatted message
-     */
-    public String trRaw(String key, Object... objects) {
-        return lang.trRaw(key, objects);
-    }
-
-    /**
-     * @return The {@link Messages} instance for this plugin
-     */
-    public Messages getLang() {
-        return lang;
-    }
-    
-    public static CommandTimer getInstance() {
-        return instance;
     }
 
 }
